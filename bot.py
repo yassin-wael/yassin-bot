116f2143a092d70835562a8a31756440c806d652
yassin-wael <35656159+yassin-wael@users.noreply.github.com>
2018-06-15 14:49:38 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready(): 
+    print("bot is ready")
+
+@client.event
+async def  on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        userID = message.author.id
+        await client.send_message(message.channel,"<@%s> pong!" % (userID))
+    if message.content.startswith('hey'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> hi" % (userID))   
+          
+
+client.run("NDUzOTcyODEyMDU5OTAxOTU0.Dfmq-w.ZTZom6PuyZtyTtXJXpLGRRH2PXw")
