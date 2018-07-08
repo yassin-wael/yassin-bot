eb29cf0d1fa0591ef9563463b0f0ebf7f9867aa2
yassin-wael <35656159+yassin-wael@users.noreply.github.com>
2018-07-09 00:29:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ client = commands.Bot(command_prefix = "!")
 @client.event
 async def on_ready(): 
     print("bot is ready")
+    await client.change_presence(game=discord.Game(name="24/7"))
     
 @client.event
 async def on_message(message):
@@ -18,7 +19,17 @@ async def on_message(message):
         await client.send_message(message.channel,"<@%s> pong!" % (userID))
     if message.content.startswith('hey'):
         userID = message.author.id
-        await client.send_message(message.channel, "<@%s> hi" % (userID))   
+        await client.send_message(message.channel, "<@%s> hi" % (userID))  
+
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+        
+
           
 
 client.run("NDUzOTcyODEyMDU5OTAxOTU0.DiPoug.qdnUKksJfrTyGTrxdJxEGy0bvC0")
