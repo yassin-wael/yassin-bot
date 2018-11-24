7784ffac5a76e0b76c0291275a13c08880cbba37
yassin-wael <35656159+yassin-wael@users.noreply.github.com>
2018-11-24 10:52:10 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="update"))
+    await client.change_presence(game=discord.Game(name="Nixon Network"))
 
 
 @client.event
